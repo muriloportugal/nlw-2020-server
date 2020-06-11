f4ab718e945164e6f4bc16c875163a13b78e36e0
Murilo Portugal <murilo.portugal@gmail.com>
2020-06-11 14:05:23 -0300

    resize next fixex

--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -13,20 +13,23 @@ const resizeImage = (request: Request, response: Response, next: NextFunction) =
       .toFile(outputFile)
       .then(file => {
         
-        if(deleteUploadedImage(inputFile)) {
-          fs.rename(outputFile,inputFile, (error) => {
-            if(error){
-              console.log(`Erro ao renomear imagem ${error}`);
-            }
-          });
-        };
-        next();
+        if(!deleteUploadedImage(inputFile)) {
+          console.log(`Erro ao deletar imagem original`);
+          return response.status(500).json({error:`Error to delete original image`});
+        }
+        fs.rename(outputFile,inputFile, (error) => {
+          if(error){
+            console.log(`Erro ao renomear imagem ${error}`);
+            return response.status(500).json({error:`Rename image error. ${error.message}.`});
+          }
+        });
+        return next();
       })
       .catch(error => {
         return response.status(500).json({error:`Resize image error. ${error.message}.`});
       });
   }
-  //return next();
+  return next();
   
 };
 
